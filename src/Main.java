996bf5621cc70ae0d249cc613db2e744acd6a5c9
Иван <triger080@mail.ru>
2022-11-06 14:02:00 +0300

    Задание 4.1

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,107 @@ public class Main {
             }
         }
 
+        //Задача 4
+        System.out.println("Задача 4");
+        int age4 = 20;
+        if (age4 >= 18) {
+            System.out.println("Поздравлем с совершеннолетием");
+        } else {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+
+        int age5 = 15;
+        if (age5 >= 18) {
+            System.out.println("Поздравлем с совершеннолетием");
+        } else {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+
+        //Задача 5
+        System.out.println("Задача 5");
+        int age6 = 24;
+        if (age6 >= 7) {
+            System.out.println("Ребенок ходит в школу");
+        } else {
+            System.out.println("Ребенок пока не ходит в школу");
+        }
+        int age7 = 24;
+        if (age7 >= 18) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        } else {
+            System.out.println("Человек не закончил школу и не может отправляться в университет");
+        }
+
+        int age8 = 24;
+        if (age8 >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        } else {
+            System.out.println("Человек не окончил университет и ему еще рано искать первую работу");
+        }
+
+        //Задача 6
+        System.out.println("Задача 6");
+        int currentSits1 = 60;
+        int allSits1 = 102;
+        int sittingSits1 = 60;
+
+        if (currentSits1 < sittingSits1){
+            System.out.println("Есть сидячие места");
+        }else {
+            if(currentSits1 >= allSits1){
+                System.out.println("Мест нет");
+            }else {
+                System.out.println("Есть стоячие места");
+            }
+        }
+        //Задача 7
+        System.out.println("Задача 7");
+        int age9 = 18;
+        if (age9 >= 2 && age9 <= 5) {
+            System.out.println("Если возраст человека равен " + age9 + " то ему нужно ходить в детский сад;");
+        }
+        if (age9 >= 7 && age9 <= 18) {
+            System.out.println("Если возраст человека равен " + age9 + " то ему нужно ходить в школу");
+        }
+        if (age9 > 18 && age9 <= 24) {
+            System.out.println("Если возраст человека равен " + age9 + " то его место в университете");
+        }
+        if (age9 > 24) {
+            System.out.println("Если возраст человека равен " + age9 + " то его место в университете");
+        }
+
+        //Задача 8
+        System.out.println("Задача 8");
+        int age10 = 50;
+        boolean x = age10 < 5;
+        if (x) {
+            System.out.println("Ребенок не может кататься на аттракционе");
+        }
+        boolean y = age10 >= 5 && age10 < 14;
+        if (y) {
+            System.out.println("Ребенок может кататься только в сопровождении взрослого");
+        }
+        boolean z = age10 >= 14;
+        if (z) {
+            System.out.println("Ребенок может кататься без сопровождения взрослого");
+        }
+
+
+        //Задача 9
+        System.out.println("Задача 9");
+        int one = 1;
+        int two = 2;
+        int three =3;
+
+        if (one > two && one > three){
+            System.out.println("One");
+        }
+        if (two > one && two > three) {
+            System.out.println("Two");
+        }
+        if(three > one && three > two){
+            System.out.println("Three");
+        }
 
     }
 }
